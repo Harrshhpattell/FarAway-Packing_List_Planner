192dca2aa2955ed30b2a70974d37db4cf0074d20
Patel Harsh <102842153+Harrshhpattell@users.noreply.github.com>
2023-08-03 20:22:37 +0530

    updated

--- src/swDev.js
+++ src/swDev.js
@@ -1,6 +1,10 @@
 export default function swDev() {
-  let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
-  navigator.serviceWorker.register(swUrl, { scope: "./" }).then((response) => {
-    console.warn("response", response);
-  });
+  // let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+  navigator.serviceWorker
+    .register("/FarAway-Packing_List_Planner/sw.js", {
+      scope: "/FarAway-Packing_List_Planner/",
+    })
+    .then((response) => {
+      console.warn("response", response);
+    });
 }
