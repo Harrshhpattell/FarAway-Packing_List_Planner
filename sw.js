efa455c68f2553800e7780eb5c64ec66dee2757d
Patel Harsh <102842153+Harrshhpattell@users.noreply.github.com>
2023-08-08 11:29:36 +0530

    Updates

--- sw.js
+++ sw.js
@@ -1,9 +1,42 @@
-let cacheData = "appV1";
+// let cacheData = "appV1";
 
-// to store in cache
+// // to store in cache
 
-this.addEventListener("install", (event) => {
+// this.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(cacheData).then((cache) => {
+//       cache.addAll([
+//         "/static/js/bundle.js",
+//         "/favicon.ico",
+//         "/manifest.json",
+//         "/index.html",
+//         "/ws",
+//         "/",
+//       ]);
+//     })
+//   );
+// });
+
+// // offline (fetch from cache)
+// this.addEventListener("fetch", (event) => {
+//   // check internet connection
+//   if (!navigator.onLine) {
+//     event.respondWith(
+//       caches.match(event.request).then((resp) => {
+//         if (resp) {
+//           return resp;
+//         }
+//       })
+//     );
+//   }
+// });
+
+var cacheData = "appV1";
+
+// Install event: Cache initial assets
+self.addEventListener("install", function (event) {
   event.waitUntil(
+<<<<<<< HEAD
     caches.open(cacheData).then((cache) => {
       cache.addAll([
         "/static/js/bundle.js",
@@ -14,20 +47,49 @@ this.addEventListener("install", (event) => {
         "/index.html",
         "/ws",
         "/swDev.js",
+=======
+    caches.open(cacheData).then(function (cache) {
+      return cache.addAll([
+        "/static/js/bundle.js",
+        "/favicon.ico",
+        "/manifest.json",
+        "/index.html",
+        "/ws",
+>>>>>>> main
         "/",
       ]);
     })
   );
 });
 
-// offline (fetch from cache)
-this.addEventListener("fetch", (event) => {
-  // check internet connection
+// Fetch event: Handle network requests
+self.addEventListener("fetch", function (event) {
+  // Check if the request is in the cache
+  event.respondWith(
+    caches.match(event.request).then(function (resp) {
+      if (resp) {
+        return resp; // Return cached response if available
+      }
+
+      // If not in cache, fetch the request
+      return fetch(event.request).then(function (networkResp) {
+        // Cache the fetched response for future use
+        return caches.open(cacheData).then(function (cache) {
+          cache.put(event.request, networkResp.clone());
+          return networkResp;
+        });
+      });
+    })
+  );
+});
+
+// Offline event: Handle offline requests
+self.addEventListener("fetch", function (event) {
   if (!navigator.onLine) {
     event.respondWith(
-      caches.match(event.request).then((resp) => {
+      caches.match(event.request).then(function (resp) {
         if (resp) {
-          return resp;
+          return resp; // Respond with cached data if available
         }
       })
     );
